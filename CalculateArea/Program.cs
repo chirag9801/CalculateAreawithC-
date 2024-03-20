24cce6260ec14a24aac408a73adb4ff0122e17d6
chirag9801 <chiragpatil78315@gmail.com>
2024-03-20 11:54:37 +0530

    first commit

---
+++ CalculateArea/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Area of Triangle");
+            int Base = ReadInt("enter base of triangle: ");
+            int Height = ReadInt("enter height: ");
+            int area = Area(Base, Height);
+            Console.WriteLine($"Area of triangle is {area} ");
+            
+        }
+
+        static int Area(int Base, int height)
+        {
+            return Base * height;
+
+        }
+
+
+        static int ReadInt(string message)
+        {
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
